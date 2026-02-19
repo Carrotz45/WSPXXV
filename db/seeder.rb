28b998a91bcd32fc4cea2facb6d7df77a6087cc2
Carrotz45 <anisah.johansson@gmail.com>
2026-02-19 11:32:57 +0100

    startade

--- db/seeder.rb
+++ db/seeder.rb
@@ -19,14 +19,36 @@ def drop_tables(db)
 end
 
 def create_tables(db)
-  db.execute('CREATE TABLE exempel (
+  db.execute('CREATE TABLE users (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL, 
-              description TEXT,
-              state BOOLEAN)')
+              user TEXT NOT NULL, 
+              pwd_digest TEXT NOT NULL)')
+
+  db.execute('CREATE TABLE animals (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT NOT NULL,
+              description TEXT NOT NULL,
+              age INTERGER,
+              type_of_animal TEXT NOT NULL,
+              price INTERGER,
+              user_id_of_poster INTERGER NOT NULL)')
 end
 
+animals = [
+  ["Luna", "black cheerful cat", "4", "cat", "500", "1"]
+  ["Felix", "orange lazy cat", "12", "cat", "450", "2"]
+  ["Princess", "white american pitbull", "3", "dog", "500", "3"]
+]
+
+users = [
+  ["Andreas", "kasjdkldf"]
+  ["Birgitt", "dijfsoijf"]
+  ["Clementine", "psdkfsdfkkpok"]
+]
 def populate_tables(db)
+
+  animals.each do |animal|
+    db.execute('INSERT INTO animals (name, description, age, type_of_animal, price, user_id_of_poster) VALUES (?,?,?,?,?,?)')
   db.execute('INSERT INTO exempel (name, description, state) VALUES ("Köp mjölk", "3 liter mellanmjölk, eko",false)')
   db.execute('INSERT INTO exempel (name, description, state) VALUES ("Köp julgran", "En rödgran",false)')
   db.execute('INSERT INTO exempel (name, description, state) VALUES ("Pynta gran", "Glöm inte lamporna i granen och tomten",false)')
