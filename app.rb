33d534c23d436bd276a2625dec70f06048aa577b
Carrotz45 <anisah.johansson@gmail.com>
2026-03-20 14:18:21 +0100

    update

--- app.rb
+++ app.rb
@@ -24,5 +24,12 @@ get('/upload') do
   slim(:upload)
 end
 
-post('/upload')
+post('/upload') do
+  new_name = params[:new_animal_name]
+  new_desc = params[:new_animal_description]
+  new_age = params[:new_age]
+  type_of_animal = params[:type_of_animal]
+  new_price = params[:new_price]
+
+  db.execute("INSERT INTO animals(name, description, age, type_of_animal, price) VALUES(?,?,?,?,?)", [])
 end
