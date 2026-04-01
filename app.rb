9cb6b7f3321e1bf29596d6bb8fbd25fc541501f6
Carrotz45 <anisah.johansson@gmail.com>
2026-04-01 16:21:13 +0200

    gjorde klar login upload och logout, håller på med account

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'sinatra/reloader'
 require 'BCrypt'
 
 enable :sessions
+set :session_secret, 'denna_hemliga_nyckel_maste_vara_superlang_minst_sextiofyra_tecken_annars_kraschar_rack_session_helt_och_hallet'
 
 db = SQLite3::Database.new('db/databas.db')
 db.results_as_hash = true
@@ -19,8 +20,6 @@ end
 
 get('/upload') do
 
-  
-
   slim(:upload)
 end
 
@@ -28,23 +27,30 @@ post('/upload') do
 
   user_id = session[:user_id]
 
-  if user_id.nil? 
+  p session[:user_id]
+
+  if user_id
+    new_name = params[:new_animal_name]
+    new_desc = params[:new_animal_desc]
+    new_age = params[:new_age]
+    type_of_animal = params[:type_of_animal]
+    new_price = params[:new_price]
+
+
+    db.execute("INSERT INTO animals(name, description, age, type_of_animal, price) VALUES(?,?,?,?,?)", [new_name, new_desc, new_age, type_of_animal, new_price])
+
+    animal_id = db.last_insert_row_id
+
+    #animal_id = db.execute("SELECT id FROM animals WHERE name=?", new_name)
+
+    db.execute("INSERT INTO animal_and_owner(user_id, animal_id) VALUES(?, ?)", [user_id, animal_id])
+
+    redirect('/')
+  else 
     redirect ('/login')
   end
   
-  new_name = params[:new_animal_name]
-  new_desc = params[:new_animal_description]
-  new_age = params[:new_age]
-  type_of_animal = params[:type_of_animal]
-  new_price = params[:new_price]
-
-
-  db.execute("INSERT INTO animals(name, description, age, type_of_animal, price) VALUES(?,?,?,?,?)", [new_name, new_desc, new_age, type_of_animal, new_price])
-  animal_id = db.execute("SELECT id FROM animals WHERE name=?", new_name)
-
-  db.execute("INSERT INTO animal_and_owner(user, animal) VALUES(?, ?)", [user_id, animal_id])
-
-  redirect('/')
+  
 end
 
 get('/register') do
@@ -81,7 +87,6 @@ end
 
 
 get ('/login') do
-
   slim(:login)
 end
 
@@ -93,8 +98,8 @@ post('/login') do
   result = db.execute("SELECT id, pwd_digest FROM users WHERE user=?", [username])
   
   if result.empty?
-    #username does not exist
-    redirect('/error')
+    p "username does not exist"
+    redirect('/login')
   end
   
   user_id = result.first["id"]
@@ -104,16 +109,25 @@ post('/login') do
 
     session[:user_id] = user_id
     p user_id
-    #succsessfull login
+    p session[:user_id]
+    p "succsessfull login"
 
   else
-    #unsuccsessful wrong password
+    p "unsuccsessful wrong password"
+    redirect('/login')
   end
 
   redirect('/')
 
 end
 
-get('/login') do 
-  slim(:login)
+get('/logout') do
+  session[:user_id] = nil
+  redirect('/')
 end
+
+get('/account') do
+  user_id = session[:user_id]
+end
+
+
